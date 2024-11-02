e5f3442c0bf5961b51057cd031026e09d201c9a4
cristianbrunone <cristianbrunonecmbc@gmail.com>
2024-11-02 08:06:01 -0300

    adicionando dependencias

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
-val ktor_version = "3.0.0"
-val firebase_bom_version = "33.5.1"
+val kotlin_version: String by project
+val logback_version: String by project
+val mongo_version: String by project
+val ktor_version: String by project
 
 plugins {
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.ktor)
+    kotlin("jvm") version "2.0.21"
+    id("io.ktor.plugin") version "3.0.1"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
 }
 
 group = "com.example"
@@ -22,24 +25,38 @@ repositories {
 }
 
 dependencies {
+    // Ktor server dependencies
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("io.ktor:ktor-server-auth:$ktor_version")
     implementation("io.ktor:ktor-server-auth-jwt:$ktor_version")
     implementation("io.ktor:ktor-server-websockets:$ktor_version")
+    implementation("io.ktor:ktor-server-cors:$ktor_version")
     implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
 
-    implementation("ch.qos.logback:logback-classic:1.4.10")
+    // Ktor serialization and content negotiation
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+
+    // MongoDB dependencies
+    implementation("org.mongodb:mongodb-driver-core:$mongo_version")
+    implementation("org.mongodb:mongodb-driver-sync:$mongo_version")
+    implementation("org.mongodb:bson:$mongo_version")
+
+    // Firebase dependencies
     implementation("com.google.firebase:firebase-admin:9.2.0")
     implementation("com.google.firebase:firebase-firestore:25.1.1")
     implementation("com.google.firebase:firebase-storage:21.0.1")
-    implementation("com.auth0:java-jwt:4.4.0") 
-    implementation("io.ktor:ktor-server-cors:$ktor_version")
 
+    // JWT and logging dependencies
+    implementation("com.auth0:java-jwt:4.4.0")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    // Test dependencies
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation("io.ktor:ktor-server-test-host-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
-
 
 // Configuración de la compilación de Java
 tasks.withType<JavaCompile> {
